53a0b3dc9dee5e48d1e5e2a67426116a30bb6058
Theo Archer <thste001@gmail.comclear>
2024-03-28 10:13:32 +0100

    Cool typing animation! :D

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lines = document.querySelectorAll('.pLine p');
+  let delay = 700;
+
+  lines.forEach((line, index) => {
+    const text = line.textContent;
+    const letters = text.split('');
+    line.textContent = '';
+    letters.forEach((letter, index) => {
+      setTimeout(() => {
+        line.textContent += letter;
+      }, delay + index * 100);
+    });
+    delay += letters.length * 100;
+  });
+});
